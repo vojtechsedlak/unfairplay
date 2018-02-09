28b93f45590e229e21e10aac61e8c42a54c4eabe
vojtechsedlak <sedlak.vn@gmail.com>
2018-02-08 17:29:13 -0800

    Update 2018-02-09T01:29:12.048Z

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
 	console.log("yeehaaa");
-	var data = $.get('https://docs.google.com/spreadsheets/d/e/2PACX-1vRFBkYQECaeklzVE3RzDWIJpwJa3-mh8vamy6qINnGwAfI7cgPQAFktOAqq5u3CbmC1YkK6dAaQKToa/pub?gid=0&single=true&output=csv');
-	console.log(data);
+	$('#dots').show();
+    $.ajax({url: "https://spreadsheets.google.com/feeds/list/1J9RVuxRU_5MDO1K0FAucTNN0tvf5N7XVQuDH7TR9Bgg/default/public/values?alt=json", success: function(result){
+        var data = result.feed.entry;
+        for (var i=0;i<data.length;i++) {
+        	var name = data[i].gsx$organization.$t;
+        	var logo = data[i].gsx$imagepleasedontedit.$t;
+        	var handle = data[i].gsx$twitter.$t;
+        	var tweet = encodeURI('.@'+handle+': Withdraw your support for @FairPlayCanada. You should be ashamed for supporting this Internet censorship proposal, which will harm consumers, innovation and free expression online (cc: @NavdeepSBains) http://nositeblocking.ca #DontCensorCanada #cdnpoli')
+        	$('#board').append('<div class="entity col-3"><div class="entity-image"><img src="/assets/logos/'+logo+'"></div><a class="btn" href="https://twitter.com/intent/tweet?text='+tweet+'"><img src="/assets/logos/twitter_white.svg" width:20px;"> Tweet</a></div>');
+        }
+        $('#dots').hide();
+    }});
 })
