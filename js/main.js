587d8344fd747833de7da2c6ed4a2bfc6d00336c
vojtechsedlak <sedlak.vn@gmail.com>
2018-02-08 16:33:46 -0800

    Update 2018-02-09T00:33:45.321Z

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
 $(document).ready(function() {
-	console.log("yeehaa");
+	console.log("yeehaaa");
+	var data = $.get('https://docs.google.com/spreadsheets/d/e/2PACX-1vRFBkYQECaeklzVE3RzDWIJpwJa3-mh8vamy6qINnGwAfI7cgPQAFktOAqq5u3CbmC1YkK6dAaQKToa/pub?gid=0&single=true&output=csv');
+	console.log(data);
 })
